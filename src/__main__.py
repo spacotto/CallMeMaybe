b4c5d74b00c12705cf9e6b63e4c0fe00f6771e0e
Silvia Pacotto <spacotto@c1r6p5.42lehavre.fr>
2026-05-28 18:11:47 +0200

    Fix main

---
+++ src/__main__.py
@@ -0,0 +1,55 @@
+"""
+
+Usage:
+    python3 main.py
+    Example: python3 main.py
+"""
+
+# ----------------------------------------------------------------------------
+#  Imports
+# ----------------------------------------------------------------------------
+
+import sys
+from argparse import ArgumentParser
+from utils import error, warning
+
+# ----------------------------------------------------------------------------
+#  Main entry point
+# ----------------------------------------------------------------------------
+
+def main() -> None:
+
+    try:
+        ap = ArgumentParser(description="Call Me Maybe: LLM Function Calling")
+        ap.add_argument("--functions_definition",
+                        default="data/input/functions_definition.json")
+        ap.add_argument("--input",
+                        default="data/input/function_calling_tests.json")
+        ap.add_argument("--output",
+                        default="data/output/function_calls.json")
+
+        args = ap.parse_args()
+
+        if '--functions_definition' not in args:
+            raise FileNotFoundError('No functions definition provided')
+
+    except FileNotFoundError as e:
+        error(e)
+        warning("Usage: make run ...")
+        return
+
+    except Exception as e:
+        error(e)
+        return
+
+    # Define the arguments
+
+    print(f"Loading definitions from: {args.functions_definition}")
+    print(f"Reading prompts from: {args.input}")
+    print(f"Writing outputs to: {args.output}")
+
+    # TODO: Pydantic validation, LLM SDK, and Constrained Decoder here!
+
+
+if __name__ == "__main__":
+    main()
