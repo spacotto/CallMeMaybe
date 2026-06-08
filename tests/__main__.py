1cd9e37e6540b8ffa30721e6fdca1c783879c227
Silvia Pacotto <spacotto@c1r5p11.42lehavre.fr>
2026-06-08 18:05:32 +0200

    Update testing

---
+++ tests/__main__.py
@@ -0,0 +1,33 @@
+from src.utils import Formatter as clr
+from src.utils import error as err
+
+
+def run() -> None:
+    print('\n' + ' ' + '=' * 60)
+    print(clr.apply('bold', 'white', '  Call Me Maybe: Test Suite'))
+    print(' ' + '=' * 60)
+
+    options = {
+        1: 'Visualise Tokenizer',
+        2: 'Test input validation'
+        }
+
+    print(clr.apply('bold', 'white', f'  {"n.":<3}Description'))
+    print(' ' + '-' * 60)
+
+    for k, v in options.items():
+        print(f'  {k:<3}{v}')
+
+    print(' ' + '-' * 60)
+    choice = input(clr.apply('bold', 'white', f'  Pick an option: '))
+    print()
+
+    try:
+        print()
+
+    except Exception:
+        err(' Invalid option.\n')
+
+
+if __name__ == "__main__":
+    run()
